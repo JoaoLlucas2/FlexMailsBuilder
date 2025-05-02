d35b0452abdf646bf743c2516e807c5b6eb47ad0
João Lucas <155165579+JoaoLlucas2@users.noreply.github.com>
2025-05-02 14:25:47 -0300

    First Project's Commit

---
+++ flex-mails-builder/src/main/java/com/luca/flex_mails_builder/FlexMailsBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.luca.flex_mails_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexMailsBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexMailsBuilderApplication.class, args);
+	}
+
+}
---
+++ flex-mails-builder/src/test/java/com/luca/flex_mails_builder/FlexMailsBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luca.flex_mails_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexMailsBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
